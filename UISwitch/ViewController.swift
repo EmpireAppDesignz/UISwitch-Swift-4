672909c72d09f1034a16d6379dfb5d5000c286b6
Eric Rosas <eric@empireappdesignz.com>
2019-06-11 10:25:31 -0400

    Initial Commit

---
+++ UISwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISwitch
+//
+//  Created by Eric Rosas on 6/11/19.
+//  Copyright © 2019 Eric Rosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
